338aade25d24f11bf699774c37e8917cc0b547f6
MkSibeko <68823746+MkSibeko@users.noreply.github.com>
2024-09-13 01:47:59 +0200

    plan for autoencoder

---
+++ autoencoder/state_encoder.py
@@ -0,0 +1,4 @@
+# create an auto encoder 
+# shape N - state_vars -> N/2 -> [N/4 (encoded state)] -> N/2 -> N - state_vars 
+# step find examples of SLAs 
+# make an SLA table and generate more data that is like the other SLAs 
